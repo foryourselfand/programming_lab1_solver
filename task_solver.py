2ca6a3fe7126f043e0c151665dc0ea07f0256bf4
foryourselfand <foryourselfand@gmail.com>
2019-09-27 18:20:50 +0300

    array creation

--- task_solver.py
+++ task_solver.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Union
 import re
 from task_inputs import TaskInputsCreator
 from variant_getter import VariantGetter
@@ -56,9 +56,9 @@ class FirstTaskFormatter(AbstractTaskFormatter):
         split_input = inputs.split(' ')
         print(split_input)
 
-        array_name = split_input[3]
-        array_type = split_input[5][:-1]
-        print(array_name, array_type)
+        arr_name = split_input[3]
+        arr_type = split_input[5][:-1]
+        print(arr_name, arr_type)
 
         even_flag = 'чётными' in split_input
         odd_flag = 'нечётными' in split_input
@@ -70,7 +70,25 @@ class FirstTaskFormatter(AbstractTaskFormatter):
         order = split_input[-1][:-1]
         print(order)
 
+        arr_size = 10
+        array_creation = self.create_array(arr_type, arr_name, arr_size)
+        print(array_creation)
+
         return '11\n12'
+
+    def create_array(self, arr_type: str, arr_name: str, arr_size: Union[int, List[int]]) -> str:
+        if type(arr_size) == int:
+            arr_size = [arr_size]
+        first_brackets = '[]' * len(arr_size)
+        first_part = f'{arr_type}{first_brackets} {arr_name}'
+
+        second_brackets: str = ''
+        for size in arr_size:
+            second_brackets += f'[{size}]'
+        second_part = f'new {arr_type}{second_brackets}'
+
+        result = f'{first_part} = {second_part};'
+        return result
 
 
 class SecondTaskFormatter(AbstractTaskFormatter):
